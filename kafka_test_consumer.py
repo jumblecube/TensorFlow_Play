5645826b4a2180fb3c2c00eba1891cbfa4508b00
jumblecube <jumblecubelab@gmail.com>
2018-09-26 23:37:48 -0700

    Kafka - updated with json

--- kafka_test_consumer.py
+++ kafka_test_consumer.py
@@ -1,14 +1,21 @@
 # Test kafka is running
 
-from kafka import KafkaConsumer, TopicPartition
+from kafka import KafkaConsumer
+import json
 
 print('Making connection...')
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
-consumer.subscribe(['kafkatp1'])
+consumer = KafkaConsumer('kafkatp2', bootstrap_servers='localhost:9092',
+                         auto_offset_reset='earliest')
+
+KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
 print('Getting message...')
 
+i = 0
+
 for message in consumer:
-    print(message)
+    print(str(message))
+    print(i)
+    i += 1
 
 print('Done...')
--- kafka_test_producer.py
+++ kafka_test_producer.py
@@ -2,15 +2,14 @@
 
 from kafka import KafkaProducer
 import json
+import random
 import pprint
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092'
-    # , value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
-for _ in range(100):
-    producer.send('kafkatp1', b'34748389')
-
-metrics = producer.metrics()
-pprint.pprint(metrics)
+for _ in range(10):
+    x = {'foo': 'bar'}
+    producer.send('kafkatp2', {'foo': 'bar'})
+    print(x)
